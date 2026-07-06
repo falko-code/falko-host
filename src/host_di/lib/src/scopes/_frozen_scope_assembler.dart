0382cf4e43b9173a825da832288ef7ea28fd243c
Rima Falcó <rima@falko.email>
2026-07-06 09:57:00 +0200

    Fix Global Module Key Assemble
    Signed-off-by: Rima Falcó <rima@falko.email>

--- src/host_di/lib/src/scopes/_frozen_scope_assembler.dart
+++ src/host_di/lib/src/scopes/_frozen_scope_assembler.dart
@@ -80,7 +80,7 @@ final class FrozenScopeAssembler(this._operations, this._modules) {
       final globalModuleKey = operation.source!.name + operation.key;
       for (final module in _modules) {
         _addProviderByKey(
-          module: operation.source,
+          module: module,
           key: globalModuleKey,
           provider: operation.provider,
         );
